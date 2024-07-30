a679cb2d1d576f3a18dae51b9dd42decfed33e12
freyaln <aurelien.freyling@gmail.com>
2024-07-30 21:38:58 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Change this to your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+
   await app.listen(8080);
 }
 bootstrap();
