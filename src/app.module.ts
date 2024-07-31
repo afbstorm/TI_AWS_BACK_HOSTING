94fefda5be61dcf737fc1c15f49818362e3babff
afbstorm <aurelien.freyling@bstorm.be>
2024-07-31 09:33:19 +0200

    update host

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {MoviesModule} from "./movies/movie.module";
-import { UserModule} from "./users/user.module";
-import {User} from "./users/user.entity";
-import {Movie} from "./movies/movie.entity";
-import {AuthModule} from "./auth/auth.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MoviesModule } from './movies/movie.module';
+import { UserModule } from './users/user.module';
+import { User } from './users/user.entity';
+import { Movie } from './movies/movie.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mssql',
-    host: 'mssqltechni.cliqkqm8yhny.eu-west-3.rds.amazonaws.com',
-    port: 1433,
-    username: 'admin',
-    password: 'awstechnifutur',
-    database: 'awstechni',
-    entities: [User, Movie],
-    synchronize: true,
-    options: {
-      trustServerCertificate: true
-    }
-  }),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'database-1.cliqkqm8yhny.eu-west-3.rds.amazonaws.com',
+      port: 1433,
+      username: 'admin',
+      password: 'awstechnifutur',
+      database: 'awstechni',
+      entities: [User, Movie],
+      synchronize: true,
+      options: {
+        trustServerCertificate: true,
+      },
+    }),
     UserModule,
     AuthModule,
     MoviesModule,
--- src/movies/movie.entity.ts
+++ src/movies/movie.entity.ts
@@ -2,21 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class Movie {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    distribution: string;
+  @Column()
+  distribution: string;
 
-    @Column()
-    genres: string;
+  @Column()
+  genres: string;
 
-    @Column()
-    duration: number;
+  @Column()
+  duration: number;
 }
